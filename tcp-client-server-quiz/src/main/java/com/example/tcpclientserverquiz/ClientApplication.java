c173644f0ef6be28d8f42c6c680774e979f34061
AntoniJackowski <antekjac2@gmail.com>
2026-04-21 12:40:49 +0200

    Create client window GUI

---
+++ tcp-client-server-quiz/src/main/java/com/example/tcpclientserverquiz/ClientApplication.java
@@ -0,0 +1,21 @@
+package com.example.tcpclientserverquiz;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
+
+import java.io.IOException;
+
+public class ClientApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("client-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 180);
+        stage.setTitle("CLIENT");
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+        stage.setScene(scene);
+        stage.show();
+    }
+}
